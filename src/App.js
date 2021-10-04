aaaa62d178de9d63c5083453354a8309e22c4cac
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-10-04 17:52:03 +0200

    Creation contactform jsx & css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ContactForm />   
     </div>
   );
 }
