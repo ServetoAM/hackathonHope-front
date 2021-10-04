816ad8dfec08a1d62ce6d1be0029baf2f3dac6e1
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-10-04 19:18:15 +0200

    lien entre back et front avec un testeur mailtrap

--- src/pages/ContactForm/ContactForm.jsx
+++ src/pages/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:3000/contact", {
+    let response = await fetch("http://localhost:8000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
@@ -22,22 +22,23 @@ const ContactForm = () => {
     let result = await response.json();
     alert(result.status);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
+      <form className="contactFormDiv" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nom</label>
+          <input type="text" id="name" className="nameContact" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" className="emailContact" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" className="messageContact" required />
+        </div>
+        <button type="submit">{status}</button>
+      </form>
   );
 };
 
--- src/pages/ContactForm/server.js
+++ src/pages/ContactForm/server.js
@@ -1,47 +1,48 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+// const express = require("express");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(3000, () => console.log("Server Running"));
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", router);
+// app.listen(3000, () => console.log("Server Running"));
 
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: "***************@gmail.com",
-      pass: "********",
-    },
-  });
+// var transport = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 8000,
+//     auth: {
+//       user: "203ea88d146438",
+//       pass: "4f3adf06e42d7e"
+//     }
+//   });
   
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+//   transport.verify((error) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Ready to Send");
+//     }
+//   });
 
-  router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message; 
-    const mail = {
-      from: name,
-      to: "***************@gmail.com",
-      subject: "Contact Form Submission",
-      html: `<p>Name: ${name}</p>
-             <p>Email: ${email}</p>
-             <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-      if (error) {
-        res.json({ status: "ERROR" });
-      } else {
-        res.json({ status: "Message Sent" });
-      }
-    });
-  });
+//   router.post("/contact", (req, res) => {
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const message = req.body.message; 
+//     const mail = {
+//       from: name,
+//       to: "a369f033f6-8b1aa4+1@inbox.mailtrap.io",
+//       subject: "Contact Form Submission",
+//       html: `<p>Name: ${name}</p>
+//              <p>Email: ${email}</p>
+//              <p>Message: ${message}</p>`,
+//     };
+//     transport.sendMail(mail, (error) => {
+//       if (error) {
+//         res.json({ status: "ERROR" });
+//       } else {
+//         res.json({ status: "Message Sent" });
+//       }
+//     });
+//   });
