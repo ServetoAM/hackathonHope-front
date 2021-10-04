ff16ad0b8c1d3b0eb4634070f5820fff07b0b0a6
Mo-Oblatif <martinezmoana@gmail.com>
2021-10-04 17:40:29 +0200

    Web TV style done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import WebTV from './components/pages/webTV/WebTV';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <WebTV />
+        </Switch>
+      </Router>
+      <p className="footer">© Hope Esport – 2021 Mentions légales</p>
     </div>
   );
 }
