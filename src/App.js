3ea6f00f778825245903615218367c8b0d375351
RemiGuti <koulouffpro@gmail.com>
2021-10-04 18:59:09 +0200

    routing ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import WebTV from './components/pages/webTV/WebTV';
+import WebTV from './components/pages/webTV/WebTV.jsx';
+import Home from './components/pages/accueil/Home.jsx';
+import Equipe from './components/pages/equipe/Equipe.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -8,9 +10,11 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <WebTV />
-        </Switch>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/web-tv" component={WebTV}/>
+            <Route path="/equipe" component={Equipe}/>
+          </Switch>
       </Router>
       <p className="footer">© Hope Esport – 2021 Mentions légales</p>
     </div>
