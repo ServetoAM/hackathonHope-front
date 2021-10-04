2cf3bf10537b2e7e8dc26623a1899c63c6676333
RemiGuti <koulouffpro@gmail.com>
2021-10-05 00:17:53 +0200

    players card

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,17 +8,17 @@ function Navbar() {
 	return (
 		<div className="navbar" >
 			<div className="text-style1">
-				<Link to="/" className="button">Accueil</Link>
-				<Link to="/web-tv" className="button">Web TV</Link>
-				<Link to="/equipe" className="button">Équipe Hearthstone</Link>
+				<Link to="/" className="button1">Accueil</Link>
+				<Link to="/web-tv" className="button1">Web TV</Link>
+				<Link to="/equipe" className="button1">Équipe</Link>
 			</div>
 					<span className="polygon">
 						<img className="logo" src={logo} alt="Logo Hope" />
 					</span>
 			<div className="text-style2">
-				<Link className="button">Tournois</Link>
-				<Link className="button">L'association</Link>
-				<Link className="button">Contact</Link>
+				<Link className="button2">Tournois</Link>
+				<Link className="button2">L'association</Link>
+				<Link className="button2">Contact</Link>
 			</div>
 			
 		</div>
--- src/components/pages/equipe/Equipe.jsx
+++ src/components/pages/equipe/Equipe.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PlayerCard from '../../playerCard/PlayerCard.jsx'
+import "./Equipe.css"
 
 function Equipe() {
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:8000/api/players')
+        .then((res) => res.data)
+        .then((data) => setPlayers(data))
+    }, [])
+
+
     return (
-        <div>
-            
+        <div className="Team">
+
+            {players.map((player) => (
+                <div key={player.id} className="team">
+                    <PlayerCard player={player}/>
+                </div>
+            ))}
         </div>
     )
 }
