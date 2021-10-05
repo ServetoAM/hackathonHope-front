e14b6807c51e58f487cc0b2420a0e203e714fdf1
RemiGuti <koulouffpro@gmail.com>
2021-10-06 00:47:18 +0200

    cardplayers

--- src/components/playerCard/PlayerDetails.jsx
+++ src/components/playerCard/PlayerDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import './PlayerDetails.css';
+import './PlayerDetails.scss';
+import Prout from "../../assets/prout.png";
+import { FaTwitter, FaFacebookF, FaInstagram, FaTiktok, FaTwitch, FaDiscord } from 'react-icons/fa';
 import axios from 'axios';
 
 function PlayerDetails(props) {
@@ -27,23 +29,44 @@ console.log(playerDetails)
                             <p>{playerDetails.firstname}</p>
                             <p>{playerDetails.lastname} </p>
                         </div>
+
                     <div className="main-info-container"> 
+
                         <div className="container-1">
-                            <p>{playerDetails.occupation}</p>
-                            <p>{playerDetails.age}</p>
-                            <p>{playerDetails.qualities}</p>
+                            <img src={Prout} alt="" />
+                            <ul>
+                                <li className="cardTextStyle">{playerDetails.occupation}</li>
+                                <li className="cardTextStyle">{playerDetails.age}</li>
+                                <li className="cardTextStyle">{playerDetails.qualities}</li>
+                            </ul>
                         </div>
+
                         <div className="empty">
                         </div>
+
                         <div className="container-2">
-                            <p>{playerDetails.palmares}</p>
-                            <p>{playerDetails.description_player}</p>
+                        <img src={Prout} alt="" />
+                        <ul>
+                            <li className="cardTextStyle">{playerDetails.palmares}</li>
+                            <li className="cardTextStyle">{playerDetails.description_player}</li>
+                        </ul>
                         </div>
+
                     </div>
+
                     <div className="second-info">
-                        <p>{playerDetails.other_games}</p>
-                        <p>{playerDetails.discord}</p>
-                        <p>{playerDetails.twitter}{playerDetails.facebook}{playerDetails.instagram}{playerDetails.tiktok}{playerDetails.twitch}</p>
+                    <img src={Prout} alt="" />
+                    <ul>
+                        <li className="cardTextStyle">{playerDetails.other_games}</li>
+                        <li className="cardTextStyle">{playerDetails.discord}</li>
+                        <div className="gridSocial">
+                        <li className={`${playerDetails.twitter}` === "" ? 'empty' : 'full'}><a href={playerDetails.twitter} target="blank_"><FaTwitter/></a></li>
+					<li className={`${playerDetails.twitch}` === "" ? 'empty' : 'full'}><a href={playerDetails.twitch} target="blank_"><FaTwitch/></a></li>
+					<li className={`${playerDetails.instagram}` === "" ? 'empty' : 'full'}><a href={playerDetails.instagram} target="blank_"><FaInstagram/></a></li>
+					<li className={`${playerDetails.facebook}` === "" ? 'empty' : 'full'} ><a href={playerDetails.facebook} target="blank_"><FaFacebookF/></a></li>
+					<li className={`${playerDetails.tiktok}` === "" ? 'empty' : 'full'}><a href={playerDetails.tiktok} target="blank_"><FaTiktok/></a></li>
+                    </div>
+                    </ul>
                     </div>
                 </div>
             </div>
