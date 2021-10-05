dace179d81fc7932a2ac9233a5a12932480ea14f
Mo-Oblatif <martinezmoana@gmail.com>
2021-10-05 21:14:40 +0200

    Card css done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,27 +7,23 @@ import logo from '../../assets/Logo_Hope_ESport_format_PNG.png'
 
 function Navbar() {
 	return (
-		<div className="navbar" >
+		<div className="navbar">
 			<div className="text-style1">
-
 				<Link to="/" className="button1">Accueil</Link>
 				<Link to="/web-tv" className="button1">Web TV</Link>
-
-			<div className="nav-listItem button1">Tournois<IoIosArrowDown />
+			<div className="nav-listItem button1">Tournois<IoIosArrowDown className="arrow"/>
 					<ul  className="nav-listItemDrop">
 					<Link className="text-decoration" to="/brackets"><li className="button3">Brackets</li></Link>
 					<Link className="text-decoration" to="/hall-of-fame"><li className="button3">Hall of fame</li></Link>
 						</ul>
 				</div>
-
 			</div>
 					<span className="polygon">
 						<Link to="/"><img className="logo" src={logo} alt="Logo Hope" /></Link>
 					</span>
-			<div className="text-style2">
-
+			<div className="text-style1">
 				<Link className="button2" to="/equipe">Équipe</Link>
-				<div className="nav-listItem button2">L'association<IoIosArrowDown />
+				<div className="nav-listItem button2">L'association<IoIosArrowDown className="arrow"/>
 					<ul  className="nav-listItemDrop">
 					<Link className="text-decoration" to="/a-propos"><li className="button3">A propos</li></Link>
 					<Link className="text-decoration" to="/staff"><li className="button3">Staff</li></Link>
