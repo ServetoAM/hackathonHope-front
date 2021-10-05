6b4bda3eda384e35213ef71ec415e17dec245177
Mo-Oblatif <martinezmoana@gmail.com>
2021-10-05 15:51:41 +0200

    Card style ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Navbar.scss'
-import { FaTwitter, FaDiscord } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaTwitter, FaDiscord } from 'react-icons/fa'
+import { IoIosArrowDown } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/Logo_Hope_ESport_format_PNG.png'
 
 function Navbar() {
@@ -12,7 +13,7 @@ function Navbar() {
 				<Link to="/" className="button1">Accueil</Link>
 				<Link to="/web-tv" className="button1">Web TV</Link>
 
-			<div className="nav-listItem button1">Tournois
+			<div className="nav-listItem button1">Tournois <IoIosArrowDown/>
 					<ul  className="nav-listItemDrop">
 					<Link className="text-decoration" to="/brackets"><li className="button3">Brackets</li></Link>
 					<Link className="text-decoration" to="/hall-of-fame"><li className="button3">Hall of fame</li></Link>
@@ -26,7 +27,7 @@ function Navbar() {
 			<div className="text-style2">
 
 				<Link className="button2" to="/equipe">Équipe</Link>
-				<div className="nav-listItem button2">L'association
+				<div className="nav-listItem button2">L'association <IoIosArrowDown/>
 					<ul  className="nav-listItemDrop">
 					<Link className="text-decoration" to="/a-propos"><li className="button3">A propos</li></Link>
 					<Link className="text-decoration" to="/staff"><li className="button3">Staff</li></Link>
--- src/components/pages/staff/StaffCard.jsx
+++ src/components/pages/staff/StaffCard.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import './StaffCard.scss'
+import { FaTwitter, FaFacebookF, FaInstagram, FaTiktok, FaTwitch, FaDiscord } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 
 function StaffCard({staff}) {
 	return (
 		<div className="card">
 			<div className="card-style">
-			<img className="staff-logo" alt={staff.staff_firstname} src={process.env.PUBLIC_URL +  staff.staff_name}/>
+				<img className="staff-logo" alt={staff.staff_firstname} src={process.env.PUBLIC_URL +  staff.staff_name}/>
 			<ul>
-			<li className="card-text-style">{staff.firstname}</li>
-			<li className="card-text-style">{staff.age}</li>
-		  <li className="card-text-style">{staff.position}</li>
-			<Link to={staff.discord}><li>DiscordIcon</li></Link>						
-			<li><a href={staff.twitter} target="blank_">TwitterIcon</a></li>
-				{/* 	<Link><li>{staff.facebook}</li></Link>
-					<Link><li>{staff.instagram}</li></Link>
-					<Link><li>{staff.tiktok}</li></Link>
-					<Link><li>{staff.twitch}</li></Link> */}
+				<li className="card-text-style">{staff.firstname}</li>
+				<li className="card-text-style">{staff.age}</li>
+		  	<li className="card-text-style">{staff.position}</li>
+				<li className="card-text-style">Discord : {staff.discord}</li>			
+				<div className="grid-social">	
+					<li><a href={staff.twitter} target="blank_"><FaTwitter/></a></li>
+					<li><a href={staff.facebook} target="blank_"><FaFacebookF/></a></li>
+					<li><a href={staff.instagram} target="blank_"><FaInstagram/></a></li>
+					<li><a href={staff.tiktok} target="blank_"><FaTiktok/></a></li>
+					<li><a href={staff.twitch} target="blank_"><FaTwitch/></a></li>
+				</div>
 			</ul>
 			</div>
 		</div>
