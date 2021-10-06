1511dcf72f10b377f2d13fca273bf28f142c6871
RemiGuti <koulouffpro@gmail.com>
2021-10-06 02:40:23 +0200

    players presque fini

--- src/components/playerCard/PlayerDetails.jsx
+++ src/components/playerCard/PlayerDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './PlayerDetails.scss';
 import Prout from "../../assets/prout.png";
+import Controller from "../../assets/controller.png";
+import Mylife from "../../assets/mylife.png"
 import { FaTwitter, FaFacebookF, FaInstagram, FaTiktok, FaTwitch, FaDiscord } from 'react-icons/fa';
 import axios from 'axios';
 
@@ -20,9 +22,6 @@ console.log(playerDetails)
     return (
         <div className="Main">
            <div className="panel">
-                <div className="left-panel">
-                <img className="joueur" alt={playerDetails.firstname} src={process.env.PUBLIC_URL + "/assets/" + playerDetails.picture_player}/>
-                </div>
                 <div className="right-panel">
                     <p className="citation">"{playerDetails.quotes}"</p>
                         <div className="name">
@@ -33,7 +32,7 @@ console.log(playerDetails)
                     <div className="main-info-container"> 
 
                         <div className="container-1">
-                            <img src={Prout} alt="" />
+                        <img className="joueur" alt={playerDetails.firstname} src={process.env.PUBLIC_URL + "/assets/" + playerDetails.picture_player}/>
                             <ul>
                                 <li className="cardTextStyle">{playerDetails.occupation}</li>
                                 <li className="cardTextStyle">{playerDetails.age}</li>
@@ -41,41 +40,33 @@ console.log(playerDetails)
                             </ul>
                         </div>
 
-                        <div className="empty">
-                        </div>
+                        <div className="second-info">
+                    <img src={Controller} alt="" className="joueur" />
+                    <ul>
+                        <li className="cardTextStyle">{playerDetails.other_games}</li>
+                        <li className="cardTextStyle">{playerDetails.discord}</li>
+                        
+                    </ul>
+                    </div>
 
                         <div className="container-2">
-                        <img src={Prout} alt="" />
+                        <img src={Mylife} alt="" className="joueur"/>
                         <ul>
                             <li className="cardTextStyle">{playerDetails.palmares}</li>
                             <li className="cardTextStyle">{playerDetails.description_player}</li>
                         </ul>
                         </div>
-
-                    </div>
-
-                    <div className="second-info">
-                    <img src={Prout} alt="" />
-                    <ul>
-                        <li className="cardTextStyle">{playerDetails.other_games}</li>
-                        <li className="cardTextStyle">{playerDetails.discord}</li>
-                        <div className="gridSocial">
-                        <li className={`${playerDetails.twitter}` === "" ? 'empty' : 'full'}><a href={playerDetails.twitter} target="blank_"><FaTwitter/></a></li>
+                        </div>
+                    <div className="socialIcon">
+                    <li className={`${playerDetails.twitter}` === "" ? 'empty' : 'full'}><a href={playerDetails.twitter} target="blank_"><FaTwitter/></a></li>
 					<li className={`${playerDetails.twitch}` === "" ? 'empty' : 'full'}><a href={playerDetails.twitch} target="blank_"><FaTwitch/></a></li>
 					<li className={`${playerDetails.instagram}` === "" ? 'empty' : 'full'}><a href={playerDetails.instagram} target="blank_"><FaInstagram/></a></li>
 					<li className={`${playerDetails.facebook}` === "" ? 'empty' : 'full'} ><a href={playerDetails.facebook} target="blank_"><FaFacebookF/></a></li>
 					<li className={`${playerDetails.tiktok}` === "" ? 'empty' : 'full'}><a href={playerDetails.tiktok} target="blank_"><FaTiktok/></a></li>
                     </div>
-                    </ul>
-                    </div>
+
                 </div>
             </div>
-           
-           <div className="panel">
-                <div className="left-panel">
-                    
-                </div>
-            </div>    
         </div>
     )
 }
