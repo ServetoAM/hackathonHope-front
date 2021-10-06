d57887e0bababd76c8e53af2c01fe8e86fa61329
Mo-Oblatif <martinezmoana@gmail.com>
2021-10-06 03:08:22 +0200

    Hall Of Fame container done

--- src/components/pages/tournois/hall of fame/HallFame.jsx
+++ src/components/pages/tournois/hall of fame/HallFame.jsx
@@ -3,11 +3,23 @@ import './HallFame.css'
 
 function HallFame() {
     return (
-        <div className="DivHallFame">
+        <div className="hallOfFame">
             <h1>Hall of Fame</h1>
-            <div className="TableauHallFame">
-                <div className="gagnant"></div>
-                <div className="perdant"></div>
+            <div className="tableauHallOfFame">
+                <div className="gagnant">
+                    <h2>👑 1. Winner</h2>
+                </div>
+                <div className="leaderboard">
+                    <ul>
+                        <li>2. Kirikou</li>
+                        <li>3. Super Man</li>
+                        <li>4. Flash</li>
+                        <li>5. CR7</li>
+                        <li>6. KB9</li>
+                        <li>7. Arouff</li>
+                        <li>8. Goku</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
