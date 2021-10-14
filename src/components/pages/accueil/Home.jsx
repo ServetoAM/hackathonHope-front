19869a1a2b0a61ccc41e199eef092492e20d1f3c
RemiGuti <koulouffpro@gmail.com>
2021-10-14 12:23:39 +0200

    css home & navbar

--- src/components/pages/accueil/Home.jsx
+++ src/components/pages/accueil/Home.jsx
@@ -8,36 +8,11 @@ import './Home.css'
 
 function Home() {
     return (
-        <div className="homeDiv">
-            <div className="homeSub">
-                <img className="HSLogo1" src={HS_Logo} alt="Logo HearthStone" />
-                <h1 className="homeTitle">Accueil</h1>   
-                <img className="HSLogo2" src={HS_Logo} alt="Logo HearthStone" />         
-            </div>
-
-            <div className="containerButton">
-                <div className="buttonStyle"><a href="https://discord.com/invite/UQr4dSuHAk"  target="blank_"><FaDiscord className="color1"/></a></div>
-                <div className="buttonStyle2"><a href="https://twitter.com/HopeEsportHS" target="blank_"><FaTwitter className="color2"/></a></div>
-            </div>       
-
+        <div className="homeDiv">      
 
             <p className="homeText">Hope Esport est une association esport autour du jeu HearthStone. </p>
-            
             <p className="homeText">Elle est présente sur la scène de la compétition française et européenne. Elle possède sa propre merch disponible pour tous. Les valeurs principales sont l'ambition, la performance, la communauté et l'esprit d'équipe. Hope Esport se bat #Untiltheend.</p>
-            <div className="merchandising">
-            <p>Si vous voulez nous soutenir et/ou vous faire plaisir avec notre merchandising, cliquez sur les maillots !</p>
-                <a className="homeShirt" href="https://eliminate.fr/categorie-produit/hope-esport/" target="_blank">
-                    <img className="shirt" 
-                    src={shirt_front} 
-                    alt="Tshirt Asso" 
-                    />
-                    <img className="shirt" 
-                    src={shirt_back} 
-                    alt="Tshirt Asso" 
-                    href="https://eliminate.fr/categorie-produit/hope-esport/"
-                    />
-                </a>
-            </div>
+            
         </div>
     )
 }
