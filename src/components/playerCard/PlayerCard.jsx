29470bed9ea528b0c86a82fd883b9c0262f60f36
RemiGuti <koulouffpro@gmail.com>
2021-10-24 17:06:11 +0200

    avancement page equipe

--- src/components/playerCard/PlayerCard.jsx
+++ src/components/playerCard/PlayerCard.jsx
@@ -1,19 +1,47 @@
 import "./PlayerCard.css"
 import React from 'react';
-import Controller from "../../assets/controller.png";
-// import { FaTwitter, FaFacebookF, FaInstagram, FaTiktok, FaTwitch, FaDiscord } from 'react-icons/fa';
+import { FaTwitter, FaFacebookF, FaInstagram, FaTiktok, FaTwitch, FaDiscord } from 'react-icons/fa';
 
 
 function PlayerCard({playerCard}) {
 
     return (
         <div className="container-card">   
-            <div className="card">
-                <div className="card_face">
+            <div className="player-card">
+                <div className="card-move">
+                    <div className="card_face">
                     <img className="joueur" alt={playerCard.firstname} src={process.env.PUBLIC_URL + "/assets/" + playerCard.picture_player}/>
-                </div>
-                <div className="card_face card_face-back">    
-                    <img className="joueur" alt={playerCard.firstname} src={process.env.PUBLIC_URL + "/assets/" + playerCard.picture_player_reverse}/>
+                    </div>
+                    <div className="card_face-back">    
+                        <img className="joueur" alt={playerCard.firstname} src={process.env.PUBLIC_URL + "/assets/" + playerCard.picture_player_reverse}/>
+                    
+                    <div className="irl">    
+                        <div className="irl-name">
+                            <div className="irl-name-info">{playerCard.firstname}</div>
+                            <div className="irl-name-info">{playerCard.lastname}</div>
+                            <div className="irl-name-info">{playerCard.age}</div>
+                        </div>
+                        <div className="occupation">{playerCard.occupation}</div>
+            
+                        <div className="qualities-quotes">
+                            <div className="quotes">"{playerCard.quotes}"</div>
+                            <div className="qualities">{playerCard.qualities}</div>       
+                        </div>
+                        <div>
+                            <div className="other-games">{playerCard.other_games}</div>
+                            <div className="description-player">{playerCard.description_player}</div>
+                            <div className="discord">Discord : {playerCard.discord}</div> 
+                        </div>
+                        
+                        <ul className="social-media">                 
+                            <li className={`${playerCard.twitter}` === "" ? 'empty' : 'full'}><a href={playerCard.twitter} target="blank_"><FaTwitter/></a></li>
+                            <li className={`${playerCard.twitch}` === "" ? 'empty' : 'full'}><a href={playerCard.twitch} target="blank_"><FaTwitch/></a></li>
+                            <li className={`${playerCard.instagram}` === "" ? 'empty' : 'full'}><a href={playerCard.instagram} target="blank_"><FaInstagram/></a></li>
+                            <li className={`${playerCard.facebook}` === "" ? 'empty' : 'full'} ><a href={playerCard.facebook} target="blank_"><FaFacebookF/></a></li>
+                            <li className={`${playerCard.tiktok}` === "" ? 'empty' : 'full'}><a href={playerCard.tiktok} target="blank_"><FaTiktok/></a></li>
+                        </ul>
+                        </div> 
+                    </div>
                 </div>
             </div>
         </div>
