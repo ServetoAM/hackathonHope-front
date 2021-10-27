d62212c24a1bc6bfcaa32cdd23cd89058bc06108
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-10-27 16:53:39 +0200

    début changement boutique

--- src/components/pages/boutique/Boutique.jsx
+++ src/components/pages/boutique/Boutique.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Boutique.css";
 import shirt_front from "../../../assets/shirt_front.png";
+import shirt_back from "../../../assets/shirt_back.png";
 
 function Boutique() {
   return (
@@ -12,23 +13,30 @@ function Boutique() {
         pariatur minus. Quae recusandae voluptatum labore neque nemo tempore
         incidunt, voluptates omnis magni?
       </p>
-      <img
-        className="boutiqueShirtFront"
-        src={shirt_front}
-        alt="Image Tshirt"
-      />
-      <p className="boutiqueP">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        necessitatibus non vel rerum quam illo accusantium. Maiores ab ducimus
-        fugit architecto reiciendis cum amet natus soluta, eum voluptas, aliquid
-        omnis.
-      </p>
       <a
         target="_blank"
         href="https://eliminate.fr/categorie-produit/hope-esport/"
       >
         <button className="boutiqueButton">Boutique</button>
       </a>
+      <div className="boutiqueDivShirt">
+        <img
+          className="boutiqueShirtFront"
+          src={shirt_front}
+          alt="Image Tshirt"
+        />
+        <img
+          className="boutiqueShirtFront"
+          src={shirt_back}
+          alt="Image Tshirt Derrière"
+        />
+      </div>
+      <p className="boutiqueP">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        necessitatibus non vel rerum quam illo accusantium. Maiores ab ducimus
+        fugit architecto reiciendis cum amet natus soluta, eum voluptas, aliquid
+        omnis.
+      </p>
     </div>
   );
 }
