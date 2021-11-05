eec3144001b6b823915fc1ab942dace911b0d940
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-11-05 10:03:45 +0100

    Changement du texte de l'accueil

--- src/components/pages/accueil/Home.jsx
+++ src/components/pages/accueil/Home.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import HS_Logo from '../../../assets/HS_Logo.png';
-import shirt_back from '../../../assets/shirt_back.png';
-import shirt_front from '../../../assets/shirt_front.png';
-import { FaTwitter, FaDiscord } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
-import './Home.css'
+import React from "react";
+import HS_Logo from "../../../assets/HS_Logo.png";
+import shirt_back from "../../../assets/shirt_back.png";
+import shirt_front from "../../../assets/shirt_front.png";
+import { FaTwitter, FaDiscord } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className="homeDiv">      
-
-            <p className="homeText">Hope Esport est une association esport autour du jeu HearthStone. </p>
-            <p className="homeText">Elle est présente sur la scène de la compétition française et européenne. Elle possède sa propre merch disponible pour tous. Les valeurs principales sont l'ambition, la performance, la communauté et l'esprit d'équipe. Hope Esport se bat #Untiltheend.</p>
-            
-        </div>
-    )
+  return (
+    <div className="homeDiv">
+      <p className="homeText">
+        La Hope ESport est une association française créée en 2021 qui réunit
+        des joueurs Hearthstone en une équipe compétitive.{" "}
+      </p>
+      <p className="homeText">
+        Si vous voulez découvrir nos joueurs, notre staff, nos objectifs et plein d’autres
+        choses encore, ce site est fait pour vous. Notre site web contient toutes ces informations.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/pages/apropos/Propos.jsx
+++ src/components/pages/apropos/Propos.jsx
@@ -30,60 +30,65 @@ function Propos() {
         {/* <h3 className="proposH3">
           Voici nos principaux objectifs pour l’année 2021 :
         </h3> */}
-<div className="proposCombler">
-        <p className="proposP">
-          Animés par la volonté de créer une équipe avec des valeurs
-          d’excellence, les ambitions sont claires :
-        </p>
-        <p>
-          <li>
-            Performer dans les tournois majeurs en amenant nos joueurs vers
-            l’excellence{" "}
-          </li>
-          <li>Fédérer une communauté d’abord française puis internationale </li>
-          <li>
-            Organiser des tournois afin de promouvoir et développer la scène
-            Hearthstone
-          </li>
-          <li>Devenir une structure référente dans l’univers d’Hearthstone</li>
-        </p>
-        <div className="proposImage1">
-          <img
-            className="ImageproposShake"
-            src={roulanvipere}
-            alt="Exemple Joueur"
-          />
-        </div>
+        <div className="proposCombler">
+          <p className="proposP">
+            Animés par la volonté de créer une équipe avec des valeurs
+            d’excellence, les ambitions sont claires :
+          </p>
+          <p>
+            <li>
+              Performer dans les tournois majeurs en amenant nos joueurs vers
+              l’excellence{" "}
+            </li>
+            <li>
+              Fédérer une communauté d’abord française puis internationale{" "}
+            </li>
+            <li>
+              Organiser des tournois afin de promouvoir et développer la scène
+              Hearthstone
+            </li>
+            <li>
+              Devenir une structure référente dans l’univers d’Hearthstone
+            </li>
+          </p>
+          <div className="proposImage1">
+            <img
+              className="ImageproposShake"
+              src={roulanvipere}
+              alt="Exemple Joueur"
+            />
+          </div>
         </div>
         {/* <h3 className="proposH3">
           Afin de mener à bien nos projets, voici les étapes que nous avons
           parcourues jusqu’ici :
         </h3> */}
-<div className="proposCombler">
-        <p className="proposP">
-          En seulement 6 mois, nous comptons déjà 5 qualifications aux meilleurs
-          tournois internationaux et l’organisation de deux tournois
-          communautaires.
-        </p>
+        <div className="proposCombler">
+          <p className="proposP">
+            En seulement 6 mois, nous comptons déjà 5 qualifications aux
+            meilleurs tournois internationaux et l’organisation de deux tournois
+            communautaires.
+          </p>
 
-        <p className="proposP">
-          Ayant la volonté de faire plaisir à notre communauté, notre discord se
-          développe à grande vitesse, ce qui va accélérer l’arrivée de nouveaux
-          tournois et de nos concepts originaux.
-        </p>
+          <p className="proposP">
+            Ayant la volonté de faire plaisir à notre communauté, notre discord
+            se développe à grande vitesse, ce qui va accélérer l’arrivée de
+            nouveaux tournois et de nos concepts originaux.
+          </p>
 
-        <p className="proposP">
-          En outre, notre Web TV annoncera l’arrivée de streams réguliers, pour
-          vous proposer un contenu audiovisuel, et vous faire connaître notre
-          équipe et nos joueurs. Restez à l’affût des moindres informations !
-        </p>
-        <div className="proposImage1">
-          <img
-            className="ImageproposShake"
-            src={tournoiexemple}
-            alt="Exemple tournoi"
-          />
-        </div>
+          <p className="proposP">
+            En outre, notre Web TV annoncera l’arrivée de streams réguliers,
+            pour vous proposer un contenu audiovisuel, et vous faire connaître
+            notre équipe et nos joueurs. Restez à l’affût des moindres
+            informations !
+          </p>
+          <div className="proposImage1">
+            <img
+              className="ImageproposShake"
+              src={tournoiexemple}
+              alt="Exemple tournoi"
+            />
+          </div>
         </div>
 
         {/* <h2 className="proposH2">Ambitions :</h2>
@@ -109,16 +114,20 @@ function Propos() {
           satisfaire la communauté Hearthstone.
         </p> */}
         <div className="proposCombler">
-        <p className="proposP">
-          Cependant ce n’est que le début pour notre équipe ambitieuse car après
-          tout, pourquoi devrions-nous nous fixer des limites ?
-        </p>
-        <p className="proposP">#untiltheend</p>
-        <div className="untilEnd">
-          <img src={Untiltheend} alt="banniere hope" className="untilTheEnd" />
+          <p className="proposP">
+            Cependant ce n’est que le début pour notre équipe ambitieuse car
+            après tout, pourquoi devrions-nous nous fixer des limites ?
+          </p>
+          <p className="proposP">#untiltheend</p>
+          <div className="untilEnd">
+            <img
+              src={Untiltheend}
+              alt="banniere hope"
+              className="untilTheEnd"
+            />
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
